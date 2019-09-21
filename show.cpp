9a83a990c53ec0a4ec9b7349d3f54cf4f9dbd6ad
Kume369 <952717128@qq.com>
2019-09-21 14:08:12 +0800

    show.cpp

---
+++ show.cpp
@@ -0,0 +1,23 @@
+#include"show.h" 
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start,int len){
+	int i;
+	for(i=0;i<len;i++)
+	printf("%.2x",start[i]);
+	printf("/n");
+} 
+
+void show_short(short int x){
+	show_bytes((byte_pointer) &x, sizeof(short int));
+}
+
+void show_long(long int x){
+	show_bytes((byte_pointer) &x, sizeof(long int));
+}
+
+void show_double(double x){
+	show_bytes((byte_pointer) &x,sizeof(double));
+}
