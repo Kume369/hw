58ccc67510ca1b2a3db89a5641ea0fdf32f64234
Kume369 <952717128@qq.com>
2019-09-21 14:34:17 +0800

    show.h

--- show.h
+++ show.h
@@ -1,7 +1,7 @@
 #ifndef SHOW_H
 #define SHOW_H
-#include <stdio.h>
-void show_short;
-void show_long;
-void show_double;
+#include<stdio.h>
+void show_short(short int x);
+void show_long(long int x);
+void show_double(double x);
 #endif 
